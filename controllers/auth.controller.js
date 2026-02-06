15a3f6c349c2fa8e553e131aa810c27a60767211
Blod <erkhanpiriyev@gmail.com>
2026-02-06 21:05:57 +0500

    .

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -87,7 +87,6 @@ exports.login = async (req, res) => {
 exports.getInfo = async (req, res) => {
 	try {
 		if (!req.user?.id) return res.status(401).json({ message: 'Unauthorized' })
-
 		const user = await User.findById(req.user.id).select('-password')
 		if (!user) return res.status(404).json({ message: 'User not found' })
 
